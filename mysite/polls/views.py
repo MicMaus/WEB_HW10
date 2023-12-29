bb0712879b6acee2aed3427560b6623bcee81e1b
MicMaus <michael.yasynskyi@gmail.com>
2023-12-29 18:25:56 +0100

    minor updates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.urls import reverse
 from django.contrib.auth import login
 
 from .models import Quote, Author
